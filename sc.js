119e68e38fbaf8f88610ab9498e5811a45525005
nithin <nithinbadri10.10@gmail.com>
2025-11-21 13:10:38 +0530

    initail

---
+++ sc.js
@@ -0,0 +1,19 @@
+function submitForm() {
+    let f = document.getElementById("first").value;
+    let l = document.getElementById("last").value;
+    let e = document.getElementById("email").value;
+    let p = document.getElementById("password").value;
+    let rp = document.getElementById("repassword").value;
+
+    if (!f || !l || !e || !p || !rp) {
+        alert("Fill all fields");
+        return;
+    }
+    if (p !== rp) {
+        alert("Passwords not matching");
+        return;
+    }
+
+    document.getElementById("msg").innerHTML =
+        " Registration successful!";
+}
